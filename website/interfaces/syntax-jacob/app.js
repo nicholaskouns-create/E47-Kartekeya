79df91b3d3abfd2f753230d5616c4e1c62ada300
Nicholas Kouns <nicholaskouns@gmail.com>
2026-09-18 19:17:53 -0700

    Wire cinema codec into Syntax Jacob propulsion lab

--- website/interfaces/syntax-jacob/app.js
+++ website/interfaces/syntax-jacob/app.js
@@ -1,3 +1,4 @@
+import {installCityCinemaCodec} from '../flight/city-cinema-codec.js';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
