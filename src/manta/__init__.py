4576654fa95d44548cdbe9794b8d19140897cb38
Nicholas Kouns <nicholaskouns@gmail.com>
2026-09-19 10:20:53 -0700

    Add instrument contract for manta

---
+++ src/manta/__init__.py
@@ -0,0 +1,4 @@
+"""MANTA experimental programmable-matter simulation."""
+MANTA_VERSION="0.1.0"
+from .programmable_matter import MantaEngine, MorphMode, PilotInput
+__all__=["MANTA_VERSION","MantaEngine","MorphMode","PilotInput"]
